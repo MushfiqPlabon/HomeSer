40c42c7080ffdcb155d38b60479f6dfaa38a284d
MushfiqPlabon <rahman.mushfiq2000@gmail.com>
2025-08-23 00:15:47 +0600

    Fix pyproject.toml configuration and reformat code with black and isort

--- HomeSer/views.py
+++ HomeSer/views.py
@@ -7,6 +7,7 @@ from django.core.cache import cache
 from django.db import transaction
 from django.db.models import Avg, Count, Prefetch, Sum
 from django.shortcuts import get_object_or_404, redirect, render
+
 from rest_framework import permissions, serializers, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
-import pytest
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+
+import pytest
 
 User = get_user_model()
 
